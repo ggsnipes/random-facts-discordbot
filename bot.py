b58244002dead27d3eb50c7a4f6c033a7a3d4429
ggsnipes <31529149+ggsnipes@users.noreply.github.com>
2023-03-07 10:18:41 +1100

    more tests

--- bot.py
+++ bot.py
@@ -1,20 +1,27 @@
 import discord
 import os
-    
-client = discord.client()
+from dotenv import load_dotenv
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='$')
+
+load_dotenv()
+
 
 @client.event
 async def on_ready():
     print('we have logged in as {0.user}'.format(client))
 
+@client.command()
+async def ping():
+    await client.say('pong')
+
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith('$hello'):
-        await message.channel.send('bruh...')
+    if client.user.id != message.author.id:
+        if 'test' in message.content:
+            await client.send_message(message.channel, 'testing')
 
+    await client.process_commands(message)
 
-client = MyClient(intents=intents)
 client.run(os.getenv('TOKEN'))
